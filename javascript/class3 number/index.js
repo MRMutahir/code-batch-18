de14a85aa30d51dd9ce683aec4545d7d2482cba9
MRMutahir <mutahirkareem820@gmail.com>
2025-10-27 12:56:42 +0500

    class 2 numbers done

---
+++ javascript/class3 number/index.js
@@ -0,0 +1,49 @@
+// // // // var user_num_1 = "20"
+// // // // var user_num_2 = 2
+// // // // var all_sum = user_num_1   +  user_num_2
+
+// // // // console.log("all_sum : >>>>>> ", all_sum)
+
+
+// // // // Legal variable names
+// // // var __username__ = ""
+// // // var USERNAME = ""
+// // // var username = ""
+
+// // // var username1
+// // // var username_1
+// // // var __user__name_1 
+
+// // // var $user_name = ""
+
+// // var name1 = "abc"
+// // var name1 = "efg"
+// // var name1 = "ijk"
+
+
+// // console.log(name1)
+// // console.log(name1)
+
+
+// // var user_name = "Muhammad"
+
+// // user_name = "Mutahir"
+
+// // console.log(user_name)
+
+// var spec = prompt("Your species?", "human");
+// console.log(spec)
+
+
+var number = 8
+var new_num = ++number
+console.log("1",number)
+console.log("2",new_num)
+console.log("3",number)
+
+++name 
+name++
+
+
+--name 
+name--
